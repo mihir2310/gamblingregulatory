64e7d9f7778c6bb01ce641a1288bff2b28bf443f
mihirs2310 <mihir.savkar2003@gmail.com>
2025-02-19 18:28:36 -0500

    Safeguarded API Key, added a couple functions in python files.

--- APIRequests.py
+++ APIRequests.py
@@ -1,7 +1,15 @@
 from openai import OpenAI
+import PyPDF2
+import tiktoken
+from dotenv import load_dotenv
+import os
 
-client = OpenAI(api_key="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")
+# Set up dot_env (need to get openai api key) and set up client
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=openai_api_key)
 
+# General Question Request
 def question_request(prompt):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -9,3 +17,11 @@ def question_request(prompt):
     )
     return response.choices[0].message.content.strip()
 
+# Ingest information
+def ingest_information():
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": "You need to completely make full sense of the information on this page and"}]
+    )
+    return response.choices[0].message.content.strip()
+    
--- main.py
+++ main.py
@@ -1,29 +1,14 @@
 from openai import OpenAI
 from APIRequests import question_request
+from PDFScraping import extract_page_text, count_tokens_per_page
 import PyPDF2
 import fitz
 
-def extract_page_text(file_path, page_number):
-    with open(file_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        total_pages = len(reader.pages)
-
-        # Check if the requested page exists
-        if page_number < 1 or page_number > total_pages:
-            return f"Error: Page number {page_number} is out of range. The document has {total_pages} pages."
-
-        # Extract text from the specified page (0-indexed internally)
-        page = reader.pages[page_number - 1]
-        return page.extract_text()
-
 
 if __name__ == "__main__":
-    compliance_text = extract_page_text("gambling_laws/sportsbettingregulations.pdf", 353)
-
-    #virginia_text = compliance_text
-    # response = question_request("What is the capital of France?")
-    # print(response)
-
-    print(compliance_text)
+    compliance_text = extract_page_text("gambling_laws/sportsbettingregulations.pdf", 353, 364)
+    
+    req = question_request("What is the capital of China?")
+    print(req)
 
 
